6f287e4bf4cb98c52772f96bb369479b84c07a20
Vivien Gagliano <vivien.gagliano@ensiie.fr>
2020-01-02 23:31:48 +0100

    added man page for debugger

---
+++ debugger.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+#include <unistd.h>
+#include "stack.h"
+#include "matrix.h"
+#include "cursor.h"
+#include "debugging.h"
+
+
+/* @requires : arguments shouldn't be more than 500 characters long
+ * @assigns : memory allocation is done through the interpretor program
+ * @ensures : runs the interpretor in debug mode. will not run if invalid argument (i.e. no or non-valid type argument)
+*/
+int main(int argc, char** argv) {
+    //Argument  control
+
+    if (argc<2) {
+        printf("%s is to be used with at least 1 argument, but will consider the first one only", argv[0]);
+        exit(1);
+    }
+
+    size_t lenstr = strlen(argv[1]);
+    size_t lensuffix = strlen(".p2g");
+    if (!argv[1] || lensuffix > lenstr || !strncmp(argv[1] + lenstr - lensuffix, ".p2g", lensuffix)) {
+        printf("Invalid argument format");
+        exit(1);
+    }
+
+    //Running interpretor, now in debug mode
+    char command[528] = {"DEBUG_MODE=1 ./interpretor "};
+    strcat(command, argv[1]);
+    system(command);    
+
+    return 0;
+}
